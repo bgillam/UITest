cc1fa50dfabf6afdea30bca072bd15665b1c135a
Bill Gillam <wbgillamjr@gmail.com>
2019-08-09 17:28:03 -0400

    testing onClicked methods

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("GrafDialog.fxml"));
         primaryStage.setTitle("Graph Options Panel");
-        primaryStage.setScene(new Scene(root, 375, 400));
+        primaryStage.setScene(new Scene(root, 400, 400));
         primaryStage.show();
     }
 
